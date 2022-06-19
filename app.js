0e225ab814523500293814444a4260b91aabcf40
Abelardo <abelardo.ingles@gmail.com>
2022-06-19 18:04:43 -0400

    tarea-n5

---
+++ app.js
@@ -0,0 +1,18 @@
+let n1 = 1;
+let n2 = 1;
+for(let i = 0; i < 988; i++) {
+    i = n1 + n2;
+    n1 = n2;
+    n2 = i;
+     console.log(i)
+    i % 2 ===0 ? 
+    console.log("par   " + i):
+    console.log("impar " + i);
+       
+};
+
+  // En el archivo app1.js esta la segunda parte de la tarea //
+
+
+
+
---
+++ app1.js
@@ -0,0 +1,47 @@
+// const pokemones = ['Pikachu','Charmander','Bulbasaur','Squirtle']
+
+// const upper = pokemones.map(poke => poke.toUpperCase());
+
+// console.log(upper)
+
+
+// const pokemones = [
+//     {
+//     nombre: 'Pikachu',
+//     tipo: 'Electrico'
+//     },
+//     {
+//     nombre: 'Charmander',
+//     tipo: 'Fuego',
+//     },
+//     {
+//     nombre: 'Bulbasaur',
+//     tipo: 'Planta'
+//     },
+//     {
+//     nombre: 'Squirtle',
+//     tipo: 'Agua'
+//     },
+//     {
+//     nombre: 'Charmeleon',
+//     tipo: 'Fuego'
+//     },
+//     {
+//     nombre: 'Weedle',
+//     tipo: 'Bicho'
+//     },
+//     {
+//     nombre: 'Charizard',
+//     tipo: 'Fuego'
+//     }
+//    ]
+
+//    const result = pokemones.filter(getBicho);
+
+//    function getBicho(pokemones) {
+//     return pokemones.tipo === "Bicho"
+//    }
+
+//    console.log(result)
+
+
